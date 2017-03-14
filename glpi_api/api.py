eaa8a9a6225c0164efe412fcb4419076d8a09155
celogomess <celo.gomess@gmail.com>
2017-03-13 22:00:04 -0300

    Follow the docs and create de functions. getActiveEntities

---
+++ glpi_api/api.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+class Api:
+    def __init__(self, base_url, user_token, app_token):
+        self.base_url = base_url
+        self.app_token = app_token
+        self.user_token = user_token
+
+
+    def initSession(self):
+        target_url = 'initSession/'
+        sessiondata = {'Content-Type': 'application/json',\
+                       'Authorization':'user_token '+self.user_token,'App-Token':self.app_token}
+        session = requests.get(self.base_url+target_url, headers=sessiondata)
+        self.session_token = session.json()
+        self.session_token = self.session_token['session_token']
+
+
+
+    def killSession(self):
+        target_url = 'killSession'
+        sessiondata = {'Content-Type': 'application/json', \
+                       'Session-Token':self.session_token,'App-Token':self.app_token}
+        session = requests.get(self.base_url+target_url, headers=sessiondata)
+        return(session.content)
+
+
+    def getMyProfiles(self):
+        target_url = 'getMyProfiles/'
+        sessiondata = {'Content-Type': 'application/json', \
+                       'Session-Token':self.session_token,'App-Token':self.app_token}
+        session = requests.get(self.base_url+target_url, headers=sessiondata)
+        return(session.json())
+
+    def getActiveProfile(self):
+        target_url = 'getActiveProfile/'
+        sessiondata = {'Content-Type': 'application/json', \
+                       'Session-Token':self.session_token,'App-Token':self.app_token}
+        session = requests.get(self.base_url+target_url, headers=sessiondata)
+        return(session.json())
+
+    def changeActiveProfile(self, profile_id):
+        target_url = 'getActiveProfile/'
+        sessiondata = {'Content-Type': 'application/json', \
+                       'Session-Token':self.session_token,'App-Token':self.app_token}
+        session = requests.post(self.base_url+target_url, headers=sessiondata, data=str(profile_id))
+        return(session.json())
+
+    def getMyEntities(self):
+        target_url = 'getMyEntities/'
+        sessiondata = {'Content-Type': 'application/json', \
+                       'Session-Token':self.session_token,'App-Token':self.app_token}
+        session = requests.get(self.base_url+target_url, headers=sessiondata)
+        return(session.json())
+
+    def getActiveEntities(self):
+        target_url = 'getActiveEntities/'
+        sessiondata = {'Content-Type': 'application/json', \
+                       'Session-Token':self.session_token,'App-Token':self.app_token}
+        session = requests.get(self.base_url+target_url, headers=sessiondata)
+        return(session.json())
+
+    def getTicket(self, ticket_id):
+        target_url = 'Ticket/'+str(ticket_id)+'?expand_dropdowns=true'
+        sessiondata = {'Content-Type': 'application/json',\
+                       'Session-Token': self.session_token,'App-Token': self.app_token}
+        session = requests.get(self.base_url+target_url, headers=sessiondata)
+        print(session.url)
+        return(session.content)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='glpi_api',
+    version='0.0.1',
+    packages=['requests'],
+    url='https://github.com/marcelogomess/glpi_api.git',
+    license='BSD 2',
+    author='marcelogomess',
+    author_email='celo.gomess@gmail.com',
+    description='Just a app to start with glpi api communitacion. glpi-project.org'
+)
