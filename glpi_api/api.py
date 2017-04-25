50bf974f74df342b6a775c000a85868ec8be37b6
celogomess <celo.gomess@gmail.com>
2017-04-25 16:36:29 -0300

    Add Function addItem;

--- glpi_api/api.py
+++ glpi_api/api.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from sqlite3.dbapi2 import paramstyle
-
 from requests import post, get
+
 
 class Api:
     def __init__(self, base_url, user_token, app_token):
@@ -12,115 +11,119 @@ class Api:
 
     def initSession(self):
         target_url = 'initSession/'
-        sessiondata = {'Content-Type': 'application/json',\
-                       'Authorization':'user_token '+self.user_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
+        sessiondata = {'Content-Type': 'application/json',
+                       'Authorization': 'user_token ' + self.user_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
         self.session_token = session.json()
         self.session_token = self.session_token['session_token']
 
 
-
     def killSession(self):
         target_url = 'killSession'
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.content)
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.content)
 
 
     def getMyProfiles(self):
         target_url = 'getMyProfiles/'
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def getActiveProfile(self):
         target_url = 'getActiveProfile/'
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def changeActiveProfile(self, profile_id):
         target_url = 'getActiveProfile/'
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = post(self.base_url+target_url, headers=sessiondata, data=str(profile_id))
-        return(session.json())
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = post(self.base_url + target_url, headers=sessiondata, data=str(profile_id))
+        return (session.json())
 
     def getMyEntities(self):
         target_url = 'getMyEntities/'
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def getActiveEntities(self):
         target_url = 'getActiveEntities/'
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def getFullSession(self):
         target_url = 'getFullSession/'
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
-    def getItem(self,item_type, item_id, args=None):
+    def getItem(self, item_type, item_id, args=None):
         if args is None:
-            target_url = item_type+'/'+'/'+item_id
+            target_url = item_type + '/' + '/' + item_id
         else:
-            target_url = item_type+'/'+'/'+item_id+'?'+args
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+            target_url = item_type + '/' + '/' + item_id + '?' + args
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def getAllItems(self, item_type, args=None):
         if args is None:
-            target_url = item_type+'/'
+            target_url = item_type + '/'
         else:
-            target_url = item_type+'/'+'?'+args
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+            target_url = item_type + '/' + '?' + args
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def getSubItems(self, item_type, item_id, sub_item_type, args=None):
         if args is None:
-            target_url = item_type+'/'+item_id+'/'+sub_item_type
+            target_url = item_type + '/' + item_id + '/' + sub_item_type
         else:
-            target_url = item_type+'/'+item_id+'/'+sub_item_type+'?'+args
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+            target_url = item_type + '/' + item_id + '/' + sub_item_type + '?' + args
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def getMultipleItems(self, items_dict):
         for items in items_dict['items']:
-            items_list = self.getItem(item_type='Ticket',item_id='69130',args='expand_dropdowns=true')
-        return(items_list)
+            items_list = self.getItem(item_type='Ticket', item_id='69130', args='expand_dropdowns=true')
+        return (items_list)
 
     def listSearhItemsOptions(self, item_type, args=None):
         if args is None:
-            target_url = 'listSearchOptions/'+item_type+'/'
+            target_url = 'listSearchOptions/' + item_type + '/'
         else:
-            target_url = 'listSearchOptions/'+item_type+'/'+'?'+args
+            target_url = 'listSearchOptions/' + item_type + '/' + '?' + args
 
-        sessiondata = {'Content-Type': 'application/json', \
-                       'Session-Token':self.session_token,'App-Token':self.app_token}
-        session = get(self.base_url+target_url, headers=sessiondata)
-        return(session.json())
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = get(self.base_url + target_url, headers=sessiondata)
+        return (session.json())
 
     def searchItems(self):
         return(True)
 
-    def addItems(self):
-        return(True)
+    def addItems(self, item_type, item_data):
+        target_url = item_type+'/'
+        sessiondata = {'Content-Type': 'application/json',
+                       'Session-Token': self.session_token, 'App-Token': self.app_token}
+        session = post(self.base_url + target_url, headers=sessiondata, json=item_data)
+        return (session.json())
+
 
     def updateItems(self):
         return(True)
